55a2a1e31e65dfc48ccea9acb3c595831798b822
Dan Draper <dan@cipherstash.com>
2023-10-23 01:39:00 +1100

    Start integrating latest compound index changes

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -2,21 +2,21 @@ use std::collections::HashMap;
 
 use cipherstash_client::{
     credentials::{vitur_credentials::ViturToken, Credentials},
-    encryption::{Encryption, Plaintext},
+    encryption::{Encryption, Plaintext, compound_indexer::Accumulator},
     schema::{
         column::{Index, IndexType, Tokenizer},
         ColumnConfig, TableConfig,
     },
 };
 
-use crate::{table_entry::TableEntry, CompoundAttribute, EncryptedRecord};
+use crate::{table_entry::TableEntry, CompoundAttributeOrig, EncryptedRecord, DynamoTarget};
 
 pub(crate) fn encrypted_targets<E: EncryptedRecord>(
     target: &E,
     config: &TableConfig,
 ) -> HashMap<String, Plaintext> {
     target
-        .attributes()
+        .protected_attributes()
         .iter()
         .filter_map(|(attr, plaintext)| {
             config
@@ -30,7 +30,7 @@ pub(crate) fn encrypted_targets<E: EncryptedRecord>(
 
 pub fn encrypt_composite_query<C: Credentials<Token = ViturToken>>(
     type_name: &str,
-    query: (&Plaintext, &Plaintext, &CompoundAttribute),
+    query: (&Plaintext, &Plaintext, &CompoundAttributeOrig),
     config: &TableConfig,
     cipher: &Encryption<C>,
 ) -> Result<String, Box<dyn std::error::Error>> {
@@ -38,7 +38,9 @@ pub fn encrypt_composite_query<C: Credentials<Token = ViturToken>>(
     let right_plaintext = query.1;
     let attribute = query.2;
 
-    match attribute {
+    unimplemented!();
+
+    /*match attribute {
         CompoundAttribute::Exact(left, right) => {
             let left_index_type = config
                 .get_column(left)?
@@ -88,7 +90,7 @@ pub fn encrypt_composite_query<C: Credentials<Token = ViturToken>>(
 
             Ok(term)
         }
-    }
+    }*/
 }
 
 fn get_exact_index_from_config(config: &ColumnConfig) -> Option<&IndexType> {
@@ -173,7 +175,8 @@ fn encrypt_exact_indexes<E: EncryptedRecord, C: Credentials<Token = ViturToken>>
     attributes: &HashMap<String, String>,
     entries: &mut Vec<TableEntry>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let plaintext_attributes = target.attributes();
+    let plaintext_attributes = target.protected_attributes();
+
     let type_name = E::type_name();
 
     for (attribute_name, plaintext) in plaintext_attributes.iter() {
@@ -204,6 +207,40 @@ fn encrypt_exact_indexes<E: EncryptedRecord, C: Credentials<Token = ViturToken>>
     Ok(())
 }
 
+fn encrypt_indexes<E: EncryptedRecord + DynamoTarget>(
+        parition_key: &str,
+        target: &E,
+        term_length: usize,
+        attributes: &HashMap<String, String>, // FIXME: Make a type for *encrypted attribute*
+        entries: &mut Vec<TableEntry>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+
+    for index_name in target.protected_indexes().iter() {
+        if let Some((attr, index)) = target
+            .attribute_for_index(index_name)
+            .and_then(|attr| E::index_by_name(index_name).and_then(|index| Some((attr, index)))) {
+                let key = [0; 32]; // FIXME!
+                let salt = Accumulator::from_salt(format!("{}#{}", E::type_name(), index_name));
+                index
+                    .compose_index(key, attr.try_into()?, salt)?
+                    .truncate(term_length)
+                    .terms()
+                    .into_iter()
+                    .for_each(|term| {
+                        entries.push(TableEntry {
+                            pk: parition_key.to_string(),
+                            sk: index_name.to_string(), // TODO: HMAC the sort key, too (users#index_name#pk)
+                            term: Some(hex::encode(term)),
+                            attributes: attributes.clone(),
+                        });
+                    })
+            }
+    }
+
+    Ok(())
+
+}
+
 fn encrypt_composite_indexes<E: EncryptedRecord, C: Credentials<Token = ViturToken>>(
     parition_key: &str,
     target: &E,
@@ -212,11 +249,13 @@ fn encrypt_composite_indexes<E: EncryptedRecord, C: Credentials<Token = ViturTok
     attributes: &HashMap<String, String>,
     entries: &mut Vec<TableEntry>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let plaintext_attributes = target.attributes();
+    let plaintext_attributes = target.protected_attributes();
     let type_name = E::type_name();
 
+    unimplemented!();
+
     // yikes this should probably be split up a little
-    for attribute in target.composite_attributes() {
+    /*for attribute in dbg!(target.encrypted_indexes()) {
         match attribute {
             CompoundAttribute::Exact(left, right) => {
                 let left_index_type = config
@@ -284,7 +323,7 @@ fn encrypt_composite_indexes<E: EncryptedRecord, C: Credentials<Token = ViturTok
                 }
             }
         }
-    }
+    }*/
 
     Ok(())
 }
