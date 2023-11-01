2c44d5b960d44a076a7da409f244a1c80ab483f5
Bennett Hardwick <bennett@cipherstash.com>
2023-11-01 12:59:11 +1000

    Fix up derive

--- examples/common/user.rs
+++ examples/common/user.rs
@@ -1,22 +1,18 @@
-use cipherstash_client::encryption::compound_indexer::{PrefixIndex, ExactIndex, CompoundIndex};
-use cryptonamo::{
-    traits::{Cryptonamo, EncryptedRecord, WriteConversionError, SearchableRecord},
-    Cryptonamo, Unsealed, ComposableIndex, ComposablePlaintext, Plaintext, TableAttribute,
-};
+use cryptonamo::{traits::WriteConversionError, Cryptonamo, Unsealed};
 
-#[derive(Debug)] //, Cryptonamo)]
-                 //#[cryptonamo(partition_key = "email")]
-                 //#[cryptonamo(sort_key_prefix = "user")]
+#[derive(Debug, Cryptonamo)]
+#[cryptonamo(partition_key = "email")]
+#[cryptonamo(sort_key_prefix = "user")]
 pub struct User {
-    //#[cryptonamo(query = "exact", compound = "email#name")]
-    //#[cryptonamo(query = "exact")]
+    #[cryptonamo(query = "exact", compound = "email#name")]
+    #[cryptonamo(query = "exact")]
     pub email: String,
 
-    //    #[cryptonamo(query = "prefix", compound = "email#name")]
-    //    #[cryptonamo(query = "prefix")]
+    #[cryptonamo(query = "prefix", compound = "email#name")]
+    #[cryptonamo(query = "prefix")]
     pub name: String,
 
-    //    #[cryptonamo(plaintext)]
+    #[cryptonamo(plaintext)]
     pub count: i32,
 }
 
@@ -31,61 +27,61 @@ impl User {
     }
 }
 
-impl Cryptonamo for User {
-    fn partition_key(&self) -> String {
-        self.email.clone()
-    }
+// impl Cryptonamo for User {
+//     fn partition_key(&self) -> String {
+//         self.email.clone()
+//     }
+//
+//     fn type_name() -> &'static str {
+//         "user"
+//     }
+// }
 
-    fn type_name() -> &'static str {
-        "user"
-    }
-}
-
-impl EncryptedRecord for User {
-    fn protected_attributes() -> Vec<&'static str> {
-        vec!["email", "name"]
-    }
-
-    fn plaintext_attributes() -> Vec<&'static str> {
-        vec!["count"]
-    }
-
-    fn into_unsealed(self) -> Result<Unsealed<Self>, WriteConversionError> {
-        Unsealed::new(self)
-            .protected("name", |user| Plaintext::from(&user.name))?
-            .protected("email", |user| Plaintext::from(&user.email))?
-            .plaintext("count", |user| TableAttribute::from(user.count))
-    }
-}
-
-impl SearchableRecord for User {
-    fn protected_indexes() -> Vec<&'static str> {
-        vec!["name", "email#name"]
-    }
-
-    fn index_by_name(name: &str) -> Option<Box<dyn ComposableIndex>> {
-        match name {
-            "name" => Some(Box::new(PrefixIndex::new("name", vec![]))),
-            "email#name" => Some(Box::new(
-                CompoundIndex::new(ExactIndex::new("email", vec![])).and(PrefixIndex::new(
-                    "name",
-                    vec![],
-                )),
-            )),
-            _ => None,
-        }
-    }
-
-    fn attribute_for_index(&self, index_name: &str) -> Option<ComposablePlaintext> {
-        match index_name {
-            "name" => self.name.clone().try_into().ok(),
-            "email#name" => (self.email.clone(), self.name.clone())
-                .try_into()
-                .ok(),
-            _ => None,
-        }
-    }
-}
+// impl EncryptedRecord for User {
+//     fn protected_attributes() -> Vec<&'static str> {
+//         vec!["email", "name"]
+//     }
+//
+//     fn plaintext_attributes() -> Vec<&'static str> {
+//         vec!["count"]
+//     }
+//
+//     fn into_unsealed(self) -> Result<Unsealed<Self>, WriteConversionError> {
+//         Unsealed::new(self)
+//             .protected("name", |user| Plaintext::from(&user.name))?
+//             .protected("email", |user| Plaintext::from(&user.email))?
+//             .plaintext("count", |user| TableAttribute::from(user.count))
+//     }
+// }
+//
+// impl SearchableRecord for User {
+//     fn protected_indexes() -> Vec<&'static str> {
+//         vec!["name", "email#name"]
+//     }
+//
+//     fn index_by_name(name: &str) -> Option<Box<dyn ComposableIndex>> {
+//         match name {
+//             "name" => Some(Box::new(PrefixIndex::new("name", vec![]))),
+//             "email#name" => Some(Box::new(
+//                 CompoundIndex::new(ExactIndex::new("email", vec![])).and(PrefixIndex::new(
+//                     "name",
+//                     vec![],
+//                 )),
+//             )),
+//             _ => None,
+//         }
+//     }
+//
+//     fn attribute_for_index(&self, index_name: &str) -> Option<ComposablePlaintext> {
+//         match index_name {
+//             "name" => self.name.clone().try_into().ok(),
+//             "email#name" => (self.email.clone(), self.name.clone())
+//                 .try_into()
+//                 .ok(),
+//             _ => None,
+//         }
+//     }
+// }
 
 /*impl DecryptedRecord for User {
     fn from_attributes(attributes: HashMap<String, Plaintext>) -> Self {
