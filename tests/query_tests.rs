a7b7e07e14a1ee627eefb97cd6af3a0a0a1429cc
Dan Draper <dan@cipherstash.com>
2023-11-05 14:01:21 +1100

    Added plaintext tests, reinstated license example

--- tests/query_tests.rs
+++ tests/query_tests.rs
@@ -14,13 +14,21 @@ pub struct User {
     #[cryptonamo(query = "prefix", compound = "email#name")]
     #[cryptonamo(query = "prefix")]
     pub name: String,
+
+    #[cryptonamo(plaintext)]
+    pub tag: String,
+
+    #[cryptonamo(skip)]
+    pub temp: bool,
 }
 
 impl User {
-    fn new(email: impl Into<String>, name: impl Into<String>) -> Self {
+    fn new(email: impl Into<String>, name: impl Into<String>, tag: impl Into<String>) -> Self {
         Self {
             name: name.into(),
             email: email.into(),
+            tag: tag.into(),
+            temp: false,
         }
     }
 }
@@ -38,17 +46,17 @@ async fn run_test<F: Future<Output = ()>>(f: impl FnOnce(EncryptedTable) -> F) {
         .expect("Failed to init table");
 
     table
-        .put(User::new("dan@coderdan.co", "Dan Draper"))
+        .put(User::new("dan@coderdan.co", "Dan Draper", "blue"))
         .await
         .expect("Failed to insert Dan");
 
     table
-        .put(User::new("jane@smith.org", "Jane Smith"))
+        .put(User::new("jane@smith.org", "Jane Smith", "red"))
         .await
         .expect("Failed to insert Jane");
 
     table
-        .put(User::new("daniel@example.com", "Daniel Johnson"))
+        .put(User::new("daniel@example.com", "Daniel Johnson", "green"))
         .await
         .expect("Failed to insert Daniel");
 
@@ -66,7 +74,7 @@ async fn test_query_single_exact() {
             .await
             .expect("Failed to query");
 
-        assert_eq!(res, vec![User::new("dan@coderdan.co", "Dan Draper")]);
+        assert_eq!(res, vec![User::new("dan@coderdan.co", "Dan Draper", "blue")]);
     })
     .await;
 }
@@ -88,8 +96,8 @@ async fn test_query_single_prefix() {
         assert_eq!(
             res,
             vec![
-                User::new("dan@coderdan.co", "Dan Draper"),
-                User::new("daniel@example.com", "Daniel Johnson")
+                User::new("dan@coderdan.co", "Dan Draper", "blue"),
+                User::new("daniel@example.com", "Daniel Johnson", "green")
             ]
         );
     })
@@ -108,7 +116,7 @@ async fn test_query_compound() {
             .await
             .expect("Failed to query");
 
-        assert_eq!(res, vec![User::new("dan@coderdan.co", "Dan Draper")]);
+        assert_eq!(res, vec![User::new("dan@coderdan.co", "Dan Draper", "blue")]);
     })
     .await;
 }
@@ -118,7 +126,7 @@ async fn test_query_compound() {
 async fn test_get_by_partition_key() {
     run_test(|table| async move {
         let res: Option<User> = table.get("dan@coderdan.co").await.expect("Failed to send");
-        assert_eq!(res, Some(User::new("dan@coderdan.co", "Dan Draper")));
+        assert_eq!(res, Some(User::new("dan@coderdan.co", "Dan Draper", "blue")));
     })
     .await;
 }
@@ -144,7 +152,7 @@ async fn test_delete() {
             .send()
             .await
             .expect("Failed to send");
-        assert_eq!(res, vec![User::new("daniel@example.com", "Daniel Johnson")]);
+        assert_eq!(res, vec![User::new("daniel@example.com", "Daniel Johnson", "green")]);
 
         let res = table
             .query::<User>()
