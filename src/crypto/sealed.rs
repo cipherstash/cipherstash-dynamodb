ca029251fe52b645450971459e9bf3d96570a520
Bennett Hardwick <bennett@cipherstash.com>
2024-06-27 15:15:57 +1000

    Update round trip tests

--- src/crypto/sealed.rs
+++ src/crypto/sealed.rs
@@ -49,8 +49,7 @@ impl Sealed {
         let decryptable_attributes = T::decryptable_attributes();
 
         let mut plaintext_items: Vec<Vec<&TableAttribute>> = Vec::with_capacity(items.len());
-        let mut decryptable_items =
-            Vec::with_capacity(items.len() * decryptable_attributes.len());
+        let mut decryptable_items = Vec::with_capacity(items.len() * decryptable_attributes.len());
 
         for item in items.iter() {
             let ciphertexts = decryptable_attributes
